44953c3e8e597920de2a3c38e5baff780e2b732a
SherlockHowl <proleon4@gmail.com>
2022-03-08 11:52:28 +0530

    TimeFunction implemented

--- Milestone1/Milestone1.py
+++ Milestone1/Milestone1.py
@@ -1,12 +1,17 @@
-from collections import deque
+from concurrent.futures import thread
+import threading
 import datetime
+import time
 import yaml
 
-with open('Milestone1A.yaml', 'r') as file:
+with open('Milestone1B.yaml', 'r') as file:
     data = yaml.safe_load(file)
-output = open("Milestone1A.txt", "w")
+output = open("Milestone1B.txt", "w")
 
-def execWorkFlow(tuple, parent, log, stack):
+def TimeFunction(ExecutionTime):
+    time.sleep(ExecutionTime)
+
+def execWorkFlow(tuple, parent, log):
     time = datetime.datetime.now()
     name = tuple[0]
     fullName = ""
@@ -21,17 +26,28 @@ def execWorkFlow(tuple, parent, log, stack):
             for act in content['Activities'].items():
                 execWorkFlow(act, fullName, log)
         else:
-            
+            size = len(content['Activities'])
+            threads = []
+            for act in content['Activities'].items():
+                temp_thread = threading.Thread(target=execWorkFlow, args=(act, fullName, log,))
+                temp_thread.start()
+                threads.append(temp_thread)
+            for t in threads:
+                t.join()
     else:
         function = content['Function']
         time = datetime.datetime.now()
-        log.write(str(time)+";"+fullName+" Executing "+function+"()\n")
+        if(function == 'TimeFunction'):
+            execTime = content['Inputs']['ExecutionTime']
+            execTime = int(execTime)
+            TimeFunction(execTime)
+            log.write(str(time)+";"+fullName+" Executing "+function+"("+str(execTime)+")\n")
+        else:
+            log.write(str(time)+";"+fullName+" Executing "+function+"()\n")
     time = datetime.datetime.now()
     log.write(str(time)+";"+fullName+" Exit\n")
 
 for work in data.items():
-    stack = deque()
-    stack.append(data[0])
-    execWorkFlow(work, "", output, stack)
+    execWorkFlow(work, "", output)
 
 output.close()
