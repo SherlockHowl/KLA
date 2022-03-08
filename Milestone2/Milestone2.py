40b03850a7a6fdef8b48fd64f5eac672e521af08
SherlockHowl <proleon4@gmail.com>
2022-03-08 15:05:43 +0530

    Milestone2 complete

--- Milestone2/Milestone2.py
+++ Milestone2/Milestone2.py
@@ -12,22 +12,57 @@ with open('Milestone2A.yaml', 'r') as file:
     data = yaml.safe_load(file)
 output = open("Milestone2A.txt", "w")
 
-def TimeFunction(ExecutionTime):
-    time.sleep(0)
-
-def HandleTimeFunction(inputs, fullName, time, log):
-    key = inputs['FunctionInput']
+def CheckCondition(condition):
+    condition = condition.split()
+    key = condition[0]
+    valueOfKey = 0
     if(key[0] == '$'):
         key = key[2:-1]
         if(key not in outputEvent):
             outputEvent[key] = threading.Event()
         outputEvent[key].wait(0)
+        valueOfKey = outputStack[key]
+    else:
+        valueOfKey = int(key)
+    key = condition[2]
+    thresh = 0
+    if(key[0] == '$'):
+        key = key[2:-1]
+        if(key not in outputEvent):
+            outputEvent[key] = threading.Event()
+        outputEvent[key].wait(0)
+        thresh = outputStack[key]
+    else:
+        thresh = int(key)
+    if (condition[1] == '<'):
+        return (valueOfKey < thresh)
+    elif (condition[1] == '>'):
+        return (valueOfKey > thresh)
+    elif (condition[1] == '=>'):
+        return (valueOfKey >= thresh)
+    elif (condition[1] == '<='):
+        return (valueOfKey <= thresh)
+    elif (condition[1] == '=='):
+        return (valueOfKey == thresh)
+
+def TimeFunction(ExecutionTime):
+    time.sleep(ExecutionTime)
+
+def HandleTimeFunction(inputs, fullName, time, log):
+    key = inputs['FunctionInput']
+    out = key
+    if(key[0] == '$'):
+        key = key[2:-1]
+        if(key not in outputEvent):
+            outputEvent[key] = threading.Event()
+        outputEvent[key].wait(0)
+        out = outputStack[key]
+        out = str(out)
     execTime = inputs['ExecutionTime']
     execTime = int(execTime)
     TimeFunction(execTime)
     log.write(str(time)+";"+fullName+" Executing "+"TimeFunction(")
-    flag = False
-    log.write(key+","+str(execTime))
+    log.write(out+","+str(execTime))
     log.write(")\n")
 
 def DataLoad(input):
@@ -40,8 +75,9 @@ def DataLoad(input):
     NoOfDefects = len(data)-1
     return data, NoOfDefects
 
-def HandleDataLoad(input, id):
+def HandleDataLoad(input, id, time, log):
     DataTable, NoOfDefects = DataLoad(input)
+    log.write(str(time)+";"+id+" Executing DataLoad("+input+")\n")
     if(id+".DataTable" not in outputEvent):
         outputEvent[id+".DataTable"] = threading.Event()
     if(id+".NoOfDefects" not in outputEvent):
@@ -61,6 +97,12 @@ def execWorkFlow(tuple, parent, log):
         fullName = parent+"."+name
     log.write(str(time)+";"+fullName+" Entry\n")
     content = tuple[1]
+    if 'Condition' in content:
+        skip = CheckCondition(content['Condition'])
+        if(skip == False):
+            log.write(str(time)+";"+fullName+" Skipped\n")
+            log.write(str(time)+";"+fullName+" Exit\n")
+            return
     if(content['Type'] == 'Flow'):
         if(content['Execution'] == 'Sequential'):
             for act in content['Activities'].items():
@@ -80,8 +122,7 @@ def execWorkFlow(tuple, parent, log):
         if(function == 'TimeFunction'):
             HandleTimeFunction(content['Inputs'], fullName, time, log)
         elif(function == 'DataLoad'):
-            HandleDataLoad(content['Inputs']['Filename'], fullName)
-            log.write(str(time)+";"+fullName+" Executing "+function+"()\n")
+            HandleDataLoad(content['Inputs']['Filename'], fullName, time, log)
     time = datetime.datetime.now()
     log.write(str(time)+";"+fullName+" Exit\n")
 
